d6b29018bd6fb620cda3ff030565e6d44d9cf6fc
josseLara <josselara1@gmail.com>
2023-05-19 23:17:30 -0300

    agregar cometarios a router/comment.js

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -4,11 +4,14 @@ import { connectionDB } from "../models/NewsDB.js";
 let routerComment = Router();
 
 
+// Define the route to get all comments
 routerComment.post('/getComments', async (req, res) => {
     try {
-        let querySql = "SELECT * FROM comments";
+        // Create the SQL query to get all comments
+        const querySql = "SELECT * FROM comments";
 
-        let dataComments = await new Promise((resolve, reject) => {
+        // Execute the query and wait for it to resolve
+        const dataComments = await new Promise((resolve, reject) => {
             connectionDB.query(querySql, (error, results, fields) => {
                 if (error) {
                     reject(error);
@@ -16,37 +19,44 @@ routerComment.post('/getComments', async (req, res) => {
                     resolve(results);
                 }
             });
-        })
+        });
 
+        // Send the comments as a response
         res.json(dataComments);
     } catch (err) {
+        // If an error occurs, send an error response
         res.status(400).json({ success: false, message: err.message });
     }
-})
+});
 
+// Route to save a comment
 routerComment.post('/saveComment', async (req, res) => {
     try {
-        let { name, email, comment, created_at, news_id } = req.body;
-        let commentReceived = [name, email, comment, created_at, news_id];
-
-        let querySql = `INSERT INTO comments 
-        (id, name, email, comment, created_at, news_id) 
-        VALUES (NULL, ?, ?, ?,  ?, ?)`;
-
-        let saveComment = await new Promise((resolve, reject) => {
-            connectionDB.query(querySql, commentReceived,(error, results, fields) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(results);
-                    res.json({ success: true, message: 'saves correctly!' })
-                }
-            });
-        })
+      // Extract data from request body
+      let { name, email, comment, created_at, news_id } = req.body;
+      let commentReceived = [name, email, comment, created_at, news_id];
+  
+      // SQL query to insert comment into database
+      let querySql = `INSERT INTO comments 
+      (id, name, email, comment, created_at, news_id) 
+      VALUES (NULL, ?, ?, ?,  ?, ?)`;
+  
+      // Execute query and wait for result
+      let saveComment = await new Promise((resolve, reject) => {
+        connectionDB.query(querySql, commentReceived, (error, results, fields) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+            res.json({ success: true, message: 'Comment saved successfully!' })
+          }
+        });
+      })
     } catch (err) {
-        res.status(400).json({ success: false, message: err.message });
+      // Handle errors
+      res.status(400).json({ success: false, message: err.message });
     }
-})
+  })
 
 
 
