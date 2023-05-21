8b095ce1a347fcbeb16b25aeff7458b06e56a23a
josseLara <josselara1@gmail.com>
2023-05-20 22:06:35 -0300

    creando server y modificando client view

--- client/src/components/pure/Tag/index.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './tag.module.css';
-function Tag({txt}) {
-    return ( 
-        <button className={styles.btn_tag}>{txt}</button>
-     );
-}
-
-export default Tag;
--- client/src/helpers/convertDate.js
+++
@@ -1,7 +0,0 @@
-export default function convertDate(dateString) {
-    const date = new Date(dateString);
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    return `${hours} hours and ${minutes} minutes`;
-  }
-  
