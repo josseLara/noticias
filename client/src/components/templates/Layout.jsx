4feae3f15167b1f9438edd1a1b05dfe1c5aa0599
josseLara <josselara1@gmail.com>
2023-05-25 21:23:18 -0300

    pagina search completado

--- client/src/components/templates/Layout.jsx
+++ client/src/components/templates/Layout.jsx
@@ -3,14 +3,23 @@ import SideBar from "../organisms/SideBar";
 import TopBar from "../organisms/TopBar";
 import { styled } from "styled-components";
 import { getSearchNews } from "../../_actions/news_action";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 function Layout({ children, sideBarData, tobBarData, onCategoryNewsHandler }) {
 
     const dispatch = useDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
+    
     // -----> SEARCH <-------
     const onSearchHandler = (value) => {
         dispatch(getSearchNews(value));
+
+        if (location.pathname !== '/search') {
+            // Navigate to the search page
+            navigate('/search');
+          }
     };
 
     return (
